b6f90bfb4ade86eabaaca2b73054a2ba4f7dab9d
felipe castro <pipec800@gmail.com>
2015-07-03 16:33:18 -0300

    version7

--- src/cl/pipec/applocalizar/BuscarFragment.java
+++ src/cl/pipec/applocalizar/BuscarFragment.java
@@ -1,10 +1,13 @@
 package cl.pipec.applocalizar;
 
-
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Locale;
 
 import cl.pipec.applocalizar.clases.DataBase;
+import cl.pipec.applocalizar.clases.GeoDatos;
+import cl.pipec.applocalizar.clases.ListaGeoAdapter;
 import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.os.Bundle;
@@ -17,6 +20,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class BuscarFragment extends Fragment {
 	DataBase db;
@@ -24,6 +29,7 @@ public class BuscarFragment extends Fragment {
 	EditText fechaBuscar;
 	private DatePickerDialog dpd_fecha;
 	private SimpleDateFormat dateFormatter;
+	ListView lv_resultado;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,6 +38,9 @@ public class BuscarFragment extends Fragment {
 				false);
 
 		btn_buscar = (Button) rootView.findViewById(R.id.btn_buscar);
+		lv_resultado = (ListView) rootView.findViewById(R.id.lv_buscar_result);
+		// fecha
+		dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
 		fechaBuscar = (EditText) rootView.findViewById(R.id.etxt_fromdate);
 		fechaBuscar.setInputType(InputType.TYPE_NULL);
 		fechaBuscar.setOnClickListener(new OnClickListener() {
@@ -44,6 +53,27 @@ public class BuscarFragment extends Fragment {
 
 		});
 		ajustarFecha();
+		btn_buscar.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				DataBase db = new DataBase(getActivity());
+				String fecha = fechaBuscar.getText().toString().trim();
+				String[] parts = fecha.split("-");
+				String fechaSql = parts[2] +"-"+parts[1]+"-"+parts[0];
+				ArrayList<GeoDatos> listado = db.BuscarLugaresFecha(fechaSql);
+				if (listado.isEmpty()) {
+					Toast.makeText(getActivity(), "sin datos",
+							Toast.LENGTH_LONG).show();
+				} else {
+					fechaBuscar.setText("");
+					lv_resultado.setAdapter(new ListaGeoAdapter(getActivity(),
+							listado));
+				}
+
+			}
+		});
 		return rootView;
 	}
 
@@ -58,13 +88,12 @@ public class BuscarFragment extends Fragment {
 						// TODO Auto-generated method stub
 						Calendar newDate = Calendar.getInstance();
 						newDate.set(year, monthOfYear, dayOfMonth);
-						fechaBuscar.setText(dateFormatter.format(newDate.getTime()));
+						fechaBuscar.setText(dateFormatter.format(newDate
+								.getTime()));
 					}
 				}, newCalendar.get(Calendar.YEAR),
 				newCalendar.get(Calendar.MONTH),
 				newCalendar.get(Calendar.DAY_OF_MONTH));
-
-		
 
 	}
 
